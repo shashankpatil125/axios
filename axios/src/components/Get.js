dbe712c6c7c92e2f591155a76d36d7952a60dc2b
shashank lalit patil <shashankpatil125@gmail.com>
2022-03-19 19:43:34 +0000

    mew

--- axios/src/components/Get.js
+++ axios/src/components/Get.js
@@ -1,13 +1,13 @@
 import React, {useState} from 'react'
 import axios from 'axios';
 import useSound from 'use-sound';
-import boopSfx from './N56BNFY-click (1).mp3';
+import boopSfx from './mixkit-cartoon-little-cat-meow-91.wav';
 
 
 
 function Get() {
 
-  const someAudioFile = 'axios/src/components/N56BNFY-click (1).mp3';
+  
   const [len, setlen] = useState([])
   const [border, setborder] = useState([])
   let [random,setrandom]=useState([]);
@@ -32,7 +32,7 @@ function Get() {
   return (
     <div className='text-center mx-auto my-auto'>
     <div className='border-2 rounded-lg bg-amber-400 mx-auto my-20 w-96'>
-    <button onClick={getfact} className=" my-2 bg-neutral-800 hover:bg-neutral-700 text-white font-bold py-2 px-4 border-b-4  border-black hover:border-blue-500 rounded">GET FACT</button> 
+    <button onClick={getfact} className=" my-2 bg-neutral-800 hover:bg-neutral-700 text-white font-bold py-2 px-4 border-b-4  border-black hover:border-blue-500 rounded">GET FACT 😼</button> 
      
      <div className={border}>
      <div className='bg-neutral-900 text-lg text-white   rounded-xl '>
